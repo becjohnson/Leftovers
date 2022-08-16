7b19d56e9a97493e5b4d95ed92dd478cd3684c13
Bec Johnson <bec@alienoutfits.com>
2022-08-16 10:29:34 -0400

    Created Cart & Leftover Models

---
+++ Leftovers/Models/Cart.cs
@@ -0,0 +1,18 @@
+﻿namespace Leftovers.Models
+{
+    public class Cart
+    {
+        public int CartId { get; set; }
+        public ApplicationUser? Provider { get; set; }
+
+        public string ProviderName { get; set; }
+
+
+        public ApplicationUser? Claimer { get; set; }
+
+        public string ClaimerName { get; set; }
+        
+
+        public DateTime TransactionDate { get; set; }
+    }
+}
---
+++ Leftovers/Models/Leftover.cs
@@ -0,0 +1,25 @@
+﻿namespace Leftovers.Models
+{
+    public class Leftover
+    {
+        public int LeftoverId { get; set; }
+
+        public string Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Address { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
